6bdb3522abceeb4d33944f62fa2cc9d2d7046a36
Moiseev Maxim (streambuf) <streambuf@mail.ru>
2015-04-03 02:44:18 +0300

    Report was added

--- cache.cpp
+++ cache.cpp
@@ -2,23 +2,21 @@
 #include <algorithm>
 #include <ctime>
 #include <chrono>
+#include <fstream>
 
 #define REPEAT20(x) x x x x x x x x x x x x x x x x x x x x
 
-
 namespace {
     unsigned const INITIAL_SIZE_MEMORY = 1024;
-    unsigned const FINAL_SIZE_MEMORY = 10 * 1024 * 1024;
+    unsigned const FINAL_SIZE_MEMORY = 5 * 1024 * 1024;
     unsigned const NUM_REPEAT = 20 * 20 * 20;
     unsigned const NUM_CYCLES = 500;
 }
-
 
 struct Node {
 	Node* next;
     Node() : next(NULL) {}
 };
-
 
 int getRandomValue(int i) {
     return std::rand()%i;
@@ -32,7 +30,6 @@ double byteToKilobyte(double value) {
     return value / 1024;
 }
 
-
 void linkNodes(Node* list, int n, bool random = false) {
     std::vector<int> indexes;
 
@@ -45,7 +42,7 @@ void linkNodes(Node* list, int n, bool random = false) {
 
 }
 
-void measureAvgTimeAccess(Node* list) {
+double getAvgTimeAccess(Node* list) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
@@ -55,28 +52,45 @@ void measureAvgTimeAccess(Node* list) {
 
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
-    std::cout << milliToPico(elapsed_seconds.count() / (NUM_REPEAT * NUM_CYCLES));
+    return milliToPico(elapsed_seconds.count() / (NUM_REPEAT * NUM_CYCLES));
 }
-
 
 int main() {
     std::srand(unsigned(std::time(0)));
+    // clear file
+    std::fstream in("cache.csv", std::ios::in);
+    in.close();
 
-    for (long size_memory = INITIAL_SIZE_MEMORY; size_memory < FINAL_SIZE_MEMORY; size_memory *= 1.1) {
+    std::ofstream file("cache.csv");
+    if (!file.is_open()) {
+        std::cout << "File error: cache.csv";
+        return 1;
+    }
+    file << "Memory,Time1,Time2\n";
+
+    for (double size_memory = INITIAL_SIZE_MEMORY; size_memory < FINAL_SIZE_MEMORY; size_memory *= 1.1 ) {
         int num_nodes = size_memory / sizeof(Node);
-        std::cout << byteToKilobyte(size_memory) << "KB     ";
+        double size_memory_kb =  byteToKilobyte(size_memory);
+        std::cout << size_memory_kb << "KB     ";
+        file << size_memory_kb << ",";
+
+        // Sequential list
         Node* list = new Node[num_nodes];
         linkNodes(list, num_nodes);
-        measureAvgTimeAccess(list);
-        std::cout << "     ";
+        double time = getAvgTimeAccess(list);
+        std::cout << time << "     ";
+        file << time << ",";
+        free(list);
+
+        // Random list
         list = new Node[num_nodes];
         linkNodes(list, num_nodes, true);
-        measureAvgTimeAccess(list);
-        std::cout << std::endl;
+        time =  getAvgTimeAccess(list);
+        std::cout << time << std::endl;
+        file << time << "\n";
+        free(list);
     }
+    file.close();
 
 	return 0;
 }
-
-
-	
